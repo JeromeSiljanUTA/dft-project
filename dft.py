a8672b5ee8488ac3b84440e4eda5994548087e76
Jerome <jerome.siljan@mavs.uta.edu>
2022-11-08 22:18:15 -0600

    docs(dft.py): Removed comments and added proper docstrings

--- dft.py
+++ dft.py
@@ -1,5 +1,5 @@
 """
-dft.py takes audio files of DTMF presses, runs them through a DFT and identifies the key pressed.
+dft.py takes a wav file of DTMF presses, runs them through a DFT and identifies the key pressed.
 """
 
 # imports
@@ -13,19 +13,35 @@ from scipy.signal import find_peaks
 
 
 def identify(maxima):
-    """identifies keypress"""
+    """Takes maxima as an argument and finds the closest recognized frequency.
+
+    Parameters:
+    maxima: list of x values of two highest peaks (two most promiment frequencies)
+
+    Returns: 
+    keypad value that corresponds with key pressed"""
 
     low_freqs = np.array([697, 770, 852, 941])
     high_freqs = np.array([1209, 1336, 1477, 1633])
     close_low = low_freqs[(np.abs(low_freqs - maxima[0])).argmin()]
     close_high = high_freqs[(np.abs(high_freqs - maxima[1])).argmin()]
 
-    # print(f"{sound}: {maxima[0]}, {maxima[1]}: {keypad[(close_low, close_high)]}")
     return keypad[(close_low, close_high)]
 
 
 def find_maxima(y_vals, peaks):
-    ''' finds maxima post transform '''
+    """Finds the two frequencies that make up the beep.
+
+    Takes y_vals and peaks (x values of maxima) and picks two largest values.
+
+    Parameters:
+    y_vals: values of peaks identified
+    peaks: x values (frequencies) of peaks in DFT
+
+    Returns:
+    x values of highest peaks.
+
+    """
     peak_y = [y_vals[peak] for peak in peaks]
 
     arr = [0, 0]
@@ -36,6 +52,26 @@ def find_maxima(y_vals, peaks):
         peak_y.pop(index)
 
     return [min(arr), max(arr)]
+
+
+def main():
+    """Decodes wav file"""
+    for sound in sounds:
+        sampling_rate, in_wave = read(f"project data/{sound}")
+        wave_size = in_wave.size
+
+        wave = [
+            np.abs(i)
+            for i in zip(fftfreq(wave_size, 1 / sampling_rate), fft(in_wave))
+            if i[0] > 0
+        ]
+        _, y_vals = zip(*wave)
+
+        peaks, _ = find_peaks(y_vals)
+        peaks = list(peaks)
+        maxima = find_maxima(y_vals, peaks)
+        result = identify(maxima)
+        print(f"{sound} is {result}")
 
 
 sounds = os.listdir("project data")
@@ -58,27 +94,6 @@ keypad = {
     (941, 1477): "#",
     (941, 1633): "D",
 }
-
-
-def main():
-    """main function, decodes wav file"""
-    for sound in sounds:
-        sampling_rate, in_wave = read(f"project data/{sound}")
-        wave_size = in_wave.size
-
-        wave = [
-            np.abs(i)
-            for i in zip(fftfreq(wave_size, 1 / sampling_rate), fft(in_wave))
-            if i[0] > 0
-        ]
-        _, y_vals = zip(*wave)
-
-        peaks, _ = find_peaks(y_vals)
-        peaks = list(peaks)
-        maxima = find_maxima(y_vals, peaks)
-        result = identify(maxima)
-        print(f"{sound} is {result}")
-
 
 if __name__ == "__main__":
     main()
