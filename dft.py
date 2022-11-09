9e5340ed58f67ea80581b1f224c1d48cebefbdcb
Jerome <jerome.siljan@mavs.uta.edu>
2022-11-08 19:00:47 -0600

    feat(dft.py): adding key identifier

--- dft.py
+++ dft.py
@@ -1,4 +1,5 @@
 """
+dtmf
 dft takes audio files and figures out what button was pressed
 """
 
@@ -10,8 +11,32 @@ from scipy.fft import fft, fftfreq
 from scipy.signal import find_peaks
 
 
-def graph():
-    plt.cla()
+def closest(maxima):
+    low_freqs = [1209, 1336, 1477, 1633]
+    high_freqs = [697, 770, 852, 941]
+    dist = 0
+    keypad = {
+        "1": [697, 1209],
+        "2": [697, 1336],
+        "3": [697, 1477],
+        "A": [697, 1633],
+        "4": [770, 1209],
+        "5": [770, 1336],
+        "6": [770, 1477],
+        "B": [770, 1633],
+        "7": [852, 1209],
+        "8": [852, 1336],
+        "9": [852, 1477],
+        "C": [852, 1633],
+        "*": [941, 1209],
+        "0": [941, 1336],
+        "#": [941, 1477],
+        "D": [941, 1633],
+    }
+    print(f"{sound}: {maxima[0]}, {maxima[1]}")
+
+
+def find_maxima():
     peak_y = [y[peak] for peak in peaks]
 
     index = peak_y.index(max(peak_y))
@@ -24,12 +49,19 @@ def graph():
     peaks.pop(index)
     peak_y.pop(index)
 
+    arr = [first[0], second[0]]
     plt.scatter(*first, color="green")
     plt.scatter(*second, color="green")
 
-    # plt.scatter(peaks, peak_y, color="orange")
+    return [min(arr), max(arr)]
+
+
+def graph():
+    plt.cla()
+    maxima = find_maxima()
+    closest(maxima)
     plt.plot(wave, color="blue")
-    plt.show()
+    # plt.show()
 
 
 sounds = os.listdir("project data")
